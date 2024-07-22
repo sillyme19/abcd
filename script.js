e04bf38abb44f6a83b9c0e3fd817a17f3f34ec6b
sillyme19 <113465714+sillyme19@users.noreply.github.com>
2024-07-22 17:55:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('studentForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted successfully!');
-    // You can add further processing here
-});
-
-document.getElementById('studentForm').addEventListener('reset', function() {
-    alert('Form reset!');
-});
